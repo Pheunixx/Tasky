5459dd99191a560dc5f88dda9f1f0676182a1ef1
Nnaemezie Juliet <ogochukwu.nnaemezie.247214@unn.edu.ng>
2025-09-15 17:16:47 +0100

    welcome image

--- lib/views/WelcomeScreen.dart
+++ lib/views/WelcomeScreen.dart
@@ -14,6 +14,7 @@ class Welcome extends StatelessWidget {
               [Color(0xffA399FF), Color(0x5A46FC)]
               )
             ),
+            child: Image.asset('assets/images/image5.png'),
           ),
           Container(
             color: Colors.white,
